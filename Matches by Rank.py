39d07cd48c71a17a8a8a911507b9a811b2c2de99
AlexeiTsybulsky <atsybuls@uwaterloo.ca>
2019-05-16 16:00:25 -0400

    Match Ids by rank, changed rank to variable

--- Matches by Rank.py
+++ Matches by Rank.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from numpy import zeros
 import champdict
 
 api_key = 'RGAPI-5478c4a3-e795-497b-9463-19f2a3c723e4'
@@ -11,8 +10,6 @@ division = 'IV'
 players = requests.get('https://na1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/{tier}/{division}?page=1&api_key={key}'.format(key=api_key,tier=tier,division=division))
 playersdata = players.json()
 
-matchesplayed = zeros([600],dtype=int)
-
 for i in playersdata[:9]:
     summoner = i["summonerName"]
     account = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{sum}?api_key={key}'.format(key=api_key,sum=summoner))
@@ -22,9 +19,5 @@ for i in playersdata[:9]:
     data = response.json()
     
     for i in data["matches"]:
-        x = i["champion"]
-        matchesplayed[x] = matchesplayed[x] + 1
-
-for i in champions:
-    if matchesplayed[i] > 0:
-        print(champdict.champions[i] + ': ' + str(matchesplayed[i]))
+        match_id = i["gameId"]
+        print(match_id)
