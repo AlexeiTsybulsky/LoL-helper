ef4656822304e6dd70f3328ee27553d9b88b0232
AlexeiTsybulsky <atsybuls@uwaterloo.ca>
2019-08-05 18:48:05 -0400

    Added Queue Dictionary

---
+++ queuedict.py
@@ -0,0 +1,10 @@
+queues = {
+        0: "Custom",
+        420: "Ranked Solo/Duo",
+        400: "Normal (Draft)",
+        430: "Normal (Blind)",
+        440: "Ranked Flex",
+        450: "ARAM",
+        460: "Twisted Treeline (Blind)",
+        470: "Twisted Treeline (Ranked Flex)"
+        }
